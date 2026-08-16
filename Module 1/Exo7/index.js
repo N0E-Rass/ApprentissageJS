0c15d107d021ba0bc73451c3e3abdcaf40084d45
N0E-Rass <rasatanirinanoela6@gmail.com>
2026-08-16 16:06:15 +0200

    Ajout  de exo7

---
+++ Module 1/Exo7/index.js
@@ -0,0 +1,7 @@
+let i=1;
+let somme =0;
+while(i<100){
+ somme = somme + i;
+    i++;
+} 
+console.log(somme = somme+i)
---
+++ Module 1/Exo7/note.md
@@ -0,0 +1,6 @@
+// Boucle while
+let compteur = 0;
+while (compteur < 3) {
+console.log(compteur);
+compteur++;
+}
