78cd146fd7849025dd50eac8f38b9b962a9b91a7
N0E-Rass <rasatanirinanoela6@gmail.com>
2026-08-16 15:57:13 +0200

    Ajout de exo2

---
+++ Module 1/Exo2/index.js
@@ -0,0 +1,11 @@
+let number =23
+let string =" Bonjour "
+const test = true;
+const user = null;
+let resultat;
+
+console.log(typeof number)
+console.log(typeof string )
+console.log(typeof test)
+console.log(typeof user)
+console.log(typeof resultat)
