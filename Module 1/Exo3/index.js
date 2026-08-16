d8c1e3cfbc0f1bfec8d6354abb6de47959de621a
N0E-Rass <rasatanirinanoela6@gmail.com>
2026-08-16 15:59:22 +0200

    Ajout de Exo3

---
+++ Module 1/Exo3/index.js
@@ -0,0 +1,8 @@
+let nene = '42'
+let number = Number(nene)
+console.log(number)
+console.log(typeof number)
+let nana = 3.14
+let chaine = nana.toString()
+console.log(chaine)
+console.log(typeof chaine)
